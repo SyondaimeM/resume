6125150ddec155d45408c4cb8f466b0b5c52e0f6
SanishMaharjan1 <m.sanish1225@gmail.com>
2022-10-23 23:35:14 -0400

    files added to site

---
+++ public/js/new/experience.js
@@ -0,0 +1,58 @@
+AOS.init();
+
+//  Work experience cards
+
+const experiencecards = document.querySelector(".experience-cards");
+const exp = [
+  {
+    title: "Software Engineer",
+    cardImage: "images/exp/tech.png",
+    place: "Null Pointers",
+    time: "(Feb, 2019 - Sept, 2022)",
+    desp: "<li>Worked to migrate Flipkart lite’s product page to AMP pages so that they load up instantly.</li> <li>Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.</li> <li>Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.</li>",
+  },
+  {
+    title: "Software Engineer",
+    cardImage: "images/exp/nullpointers.png",
+    place: "Tech Community Nepal | Stylus",
+    time: "(Apr, 2021 - Sept, 2022)",
+    desp: "<li>Worked with MOZILLA as a part of Google Summer Of Code.</li><li>Worked on Extension Activity Monitor which is a privileged extension for Firefox that uses the activityLog API (privileged API) to monitor the activities of the other installed extensions.</li><li>The activityLog API listens for logs from other installed extensions.</li>",
+  },
+  {
+    title: "Computer Teacher",
+    cardImage: "images/exp/usna.jpg",
+    place: "Ujjwal Shishu Niketan Academy",
+    time: "(Mar, 2018 - Feb, 2021)",
+    desp: "<li>Worked on the project “LTI (Learning Tools Interoperability) 2.0 standards Implementation for ekShiksha.</li><li>Designed a software that would help faculty to create a quiz using the questions from the database based on his/her choice of topics and he should be provided with various facilities and options to create a quiz of his choice.</li>",
+  },
+];
+
+const showCards2 = () => {
+  let output = "";
+  exp.forEach(
+    ({ title, cardImage, place, time, desp }) =>
+      (output += `        
+    <div class="col gaap" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="100" data-aos-duration="400"> 
+      <div class="card card1">
+        <img src="${cardImage}" class="featured-image"/>
+        <article class="card-body">
+          <header>
+            <div class="title">
+              <h3>${title}</h3>
+            </div>
+            <p class="meta">
+              <span class="pre-heading">${place}</span><br>
+              <span class="author">${time}</span>
+            </p>
+            <ol>
+              ${desp}
+            </ol>
+          </header>
+        </article>
+      </div>
+    </div>
+      `)
+  );
+  experiencecards.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards2);
